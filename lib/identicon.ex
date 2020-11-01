daf7e737b96dbadefebb385a696df5a8c2336d1a
franksparks <ferranbals@gmail.com>
2020-11-01 20:29:47 +0100

    10.- Added `build_pixel_map`, `draw_image` and `save_image` methods. Added documentation for those methods. Added pixel_map property on struct. Added egd dependencies. Added "transistor" and "transistors" png images.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,18 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for `Identicon`.
-  This program would ingest a string and turn it into a 5x5 squares identicon.
+    Documentation for `Identicon`.
+    This program would ingest a string and turn it into a 5x5 squares identicon.
   Mirrored on the center.
 
-  It will not be randomly generated, as the string will be converted into the identicon.
+    It will not be randomly generated, as the string will be converted into the identicon.
   Introducing the same string, the same identicon should be generated.
 
-  By doing so, there is no need to store the image, as the identicon can be
+    By doing so, there is no need to store the image, as the identicon can be
   generated every time on the fly.
   """
 
   @doc """
-  Main method consists on a pipeline, which processes the original string
+    Main method consists on a pipeline, which processes the original string
   `input`, and transforms it several times until the identicon is generated.
 
   1.- `input` will be set as the first argument on the pipeline.
@@ -27,6 +27,10 @@ defmodule Identicon do
   values.
 
   5.- `filter_odds_squares' removes the odd squares of the grid.
+
+  6.- Finally, it draws the identicon.
+
+  7.- And save the image.
   """
   def main(input) do
     input
@@ -34,10 +38,53 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   @doc """
-  `filter_odd_squares` method filters the grid to return the even values.
+    This method stores the identicon generated.
+  """
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+
+  @doc """
+    This function draws the identicon.
+    It creates an empty image and then fills it according
+    to the color and the location of the even squares.
+  """
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  @doc """
+    In order to draw the grid, some calculations have to be done
+  to pinpoint the different squares.
+  """
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map }
+  end
+
+  @doc """
+    `filter_odd_squares` method filters the grid to return the even values.
   """
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
     grid = Enum.filter grid, fn({code, _index}) ->
@@ -47,19 +94,19 @@ defmodule Identicon do
     %Identicon.Image{image | grid: grid}
   end
   @doc """
-  This function processes the 15 first values of the image and tranfoms them into 25,
+    This function processes the 15 first values of the image and tranfoms them into 25,
   following the given requirements.
 
-  To do so, first of all 'image' struct is accessed, in particular to the hex property,
+    - To do so, first of all 'image' struct is accessed, in particular to the hex property,
   the list of 16 hex numbers.
 
-  That list is divided into several lists of 3 elements, discarting the last value.
+    - That list is divided into several lists of 3 elements, discarting the last value.
 
-  `mirror_row` method is referenced to append the values to mirror every row.
+    - `mirror_row` method is referenced to append the values to mirror every row.
 
-  Flattening the list of lists, one single list with the 25 values will be returned.
+    - Flattening the list of lists, one single list with the 25 values will be returned.
 
-  `Enum.with_index` gets every element in the list and returns a tuple with
+    - `Enum.with_index` gets every element in the list and returns a tuple with
   the element and its index.
   """
   def build_grid(%Identicon.Image{hex: hex} = image) do
@@ -74,7 +121,7 @@ defmodule Identicon do
   end
 
   @doc """
-  This function appends two values to the list, to mirror the values as per needed
+    This function appends two values to the list, to mirror the values as per needed
   for the identicon.
   """
   def mirror_row(row) do
@@ -85,12 +132,12 @@ defmodule Identicon do
   @doc """
     This function gets the first thee values of the string, the RGB values.
 
-    By pattern matching, the first 3 values of `image` are stored.
+    - By pattern matching, the first 3 values of `image` are stored.
 
-    The underscore on `_tail` avoid warning messages to be displayed due to
+    - The underscore on `_tail` avoid warning messages to be displayed due to
     the fact tail is not being used anywhere.
 
-    `color` returns a tuple with RGB.
+    - `color` returns a tuple with RGB.
 
   ## Example
       # iex>Identicon.pick_color(%Identicon.Image{hex:
@@ -103,9 +150,9 @@ defmodule Identicon do
   end
 
   @doc """
-  `Hash_input` function converts a given `input` into a MD5 hash, and then
+    `Hash_input` function converts a given `input` into a MD5 hash, and then
   transforms that unique numbers into a string of 16 numbers, using the pipe operator.
-  The string of numbers is returned as a struct.
+    The string of numbers is returned as a struct.
 
   ## Example
       iex>Identicon.hash_input("transistor")
@@ -118,6 +165,5 @@ defmodule Identicon do
 
     %Identicon.Image{hex: hex}
   end
-
 
 end
--- lib/image.ex
+++ lib/image.ex
@@ -6,7 +6,8 @@ defmodule Identicon.Image do
   Is stores a list of numbers as a struct with the following properties:
   - `hex` (hexadecimal) -> with default value `nil`.
   - `color` -> color of the identicon.
-  - `grid` -> values and index of the grid
+  - `grid` -> values and index of the grid.
+  - `pixel_map` -> tuple with the top left and bottom right location of the squares to fill.
   """
-  defstruct hex: nil, color: nil, grid: nil
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 end
